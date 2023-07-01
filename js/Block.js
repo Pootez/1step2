c67612494d392747736c4b63f5e73bab7c8a6a0a
Pootez <sondrenoe@gmail.com>
2023-07-01 18:47:16 +0200

    Rework block classes

--- js/Block.js
+++ js/Block.js
@@ -1,14 +1,19 @@
+const blocks = new Map([
+    ['group-count', 0],
+    ['goal-count', 0]
+])
+
 /**
  * Represents a Block.
  */
 class Block {
     /**
-     * @param {string} [title=""] - The title of the block.
-     * @param {string} [description=""] - The description of the block.
+     * @param {string} [parent] - The parent of the block.
      */
-    constructor(title = "", description = "") {
-        this.title = title
-        this.description = description
+    constructor(parent = "block-home") {
+        this.parent = parent
+        this.title = ""
+        this.description = ""
         this.children = []
     }
 
@@ -21,7 +26,7 @@ class Block {
         if (index >= this.children.length) {
             return undefined
         } else {
-            return this.children[index]
+            return blocks.get(this.children[index])
         }
     }
 
@@ -30,83 +35,95 @@ class Block {
      * @return {Array} The children, or undefined if the index is invalid.
      */
     getChildren() {
-        return this.children
+        return this.children.map(id => { blocks.get(id) })
     }
 
-    /**
-     * Returns the entire family tree of the Block to a depth.
-     * @param {number|undefined} num The depth of the family tree, 0 returning just this block, negative numbers/undefined returning the entire tree.
-     * @returns {Array} The array of the tree to the leaf node, or given depth. [block, [block, leafBlock], leafBlock]
-     */
-    getTree(num = -1) {
-        if (this.children.length == 0 || num == 0) {
-            return [this]
-        }
-        let arr = [this]
-        for (let i = 0; i < this.children.length; i++) {
-            arr.push(this.children[i].getTree(num - 1))
-        }
-        return arr
-    }
+    // /**
+    //  * Returns the entire family tree of the Block to a depth.
+    //  * @param {number|undefined} num The depth of the family tree, 0 returning just this block, negative numbers/undefined returning the entire tree.
+    //  * @returns {Array} The array of the tree to the leaf node, or given depth. [block, [block, leafBlock], leafBlock]
+    //  */
+    // getTree(num = -1) {
+    //     if (this.children.length == 0 || num == 0) {
+    //         return [this]
+    //     }
+    //     let arr = [this]
+    //     for (let i = 0; i < this.children.length; i++) {
+    //         arr.push(this.children[i].getTree(num - 1))
+    //     }
+    //     return arr
+    // }
+
+    // /**
+    // * Returns a branch of a family tree, given a list of child indexes.
+    // * @param {Array} arr The array of child indexes defining the family branch.
+    // * @param {number} depth The the current index in the depth search, starts at 0.
+    // * @returns {Array|undefined} The array of children corresponding to the child indexes (starting at this block), or undefined if one or more of the indexes are invalid
+    // */
+    // getBranch(arr, depth) {
+    //     if (arr.length <= 0 || depth >= arr.length) {
+    //         return [this]
+    //     }
+    //     const index = arr[depth]
+    //     if (index >= this.children.length || index < 0) {
+    //         return undefined
+    //     }
+    //     if (arr.length == 0) {
+    //         return [this.children[index]]
+    //     }
+    //     let branch = this.children[index].getBranch(arr, depth + 1)
+    //     if (!branch) return undefined
+    //     branch.unshift(this.children[index])
+    //     return branch
+    // }
 
     /**
-    * Returns a branch of a family tree, given a list of child indexes.
-    * @param {Array} arr The array of child indexes defining the family branch.
-    * @param {number} depth The the current index in the depth search, starts at 0.
-    * @returns {Array|undefined} The array of children corresponding to the child indexes (starting at this block), or undefined if one or more of the indexes are invalid
-    */
-    getBranch(arr, depth) {
-        if (arr.length <= 0 || depth >= arr.length) {
-            return [this]
-        }
-        const index = arr[depth]
-        if (index >= this.children.length || index < 0) {
-            return undefined
-        }
-        if (arr.length == 0) {
-            return [this.children[index]]
-        }
-        let branch = this.children[index].getBranch(arr, depth + 1)
-        if (!branch) return undefined
-        branch.unshift(this.children[index])
-        return branch
-    }
-
-    /**
-     * 
      * @param {number} index The index of where the child is to be added. If undefined, it get's added at the end.
      */
     addGoal(index) {
+        const goal = newGoal(this.id)
         index && index >= 0 && index <= this.children.length
-            ? this.children.splice(index, 0, new Goal())
-            : !index && this.children.push(new Goal())
+            ? this.children.splice(index, 0, goal.id)
+            : !index && this.children.push(goal.id)
     }
 
     /**
-     * 
-     * @param {number} index The index of the child Block to be removed.
+     * Remove child at index.
+     * @param {number|string} index The index or id of the child Block to be removed recursively.
      */
     removeChild(index) {
-        index >= 0 && index < this.children.length
-            && this.children.splice(index, 1)
+        if (typeof index == 'number' && index >= 0 && index < this.children.length) {
+            const block = blocks.get(this.children[index])
+            block.removeChildren()
+            blocks.remove(this.children[index])
+            this.children.splice(index, 1)
+        }
+        else {
+            const block = blocks.get(index)
+            block.removeChildren()
+            blocks.remove(index)
+            this.children.splice(index)
+        }
     }
 
     /**
      * Sets the children to an empty array
      */
     removeChildren() {
-        this.children = []
+        for (let i = 0; i < this.children.length; i++) {
+            this.removeChild(0)
+        }
     }
 
     /**
      * Finds the number of leaf blocks of the blocks family tree.
      * @returns The number of leaf blocks of its family tree.
      */
-    numberOfLeafBlocks() {
+    get numberOfLeafBlocks() {
         if (this.children.length == 0) return 1
         let leafBlocks = 0
         for (let i = 0; i < this.children.length; i++) {
-            leafBlocks += this.children[i].numberOfLeafBlocks()
+            leafBlocks += blocks.get(this.children[i]).numberOfLeafBlocks()
         }
         return leafBlocks
     }
@@ -117,21 +134,24 @@ class Block {
  */
 class Group extends Block {
     /**
-     * @param {string} [title=""] - The title of the block.
-     * @param {string} [description=""] - The description of the block.
+     * @param {string} [parent] - The parent of the block.
      */
-    constructor(title = "", description = "") {
-        super(title, description)
+    constructor(parent) {
+        super(parent)
+        const idNum = blocks.get('group-count')
+        this.id = 'group-' + idNum
+        blocks.set('group-count', idNum + 1)
     }
 
     /**
-     * 
+     * Add group as child.
      * @param {number} index The index of where the child is to be added. If undefined, it get's added at the end.
      */
     addGroup(index) {
+        const group = new Group(this.id)
         index && index >= 0 && index <= this.children.length
-            ? this.children.splice(index, 0, new Group())
-            : !index && this.children.push(new Group())
+            ? this.children.splice(index, 0, group)
+            : !index && this.children.push(group)
     }
 }
 
@@ -140,11 +160,10 @@ class Group extends Block {
  */
 class HomeGroup extends Group {
     /**
-     * @param {string} [title=""] - The title of the block.
-     * @param {string} [description=""] - The description of the block.
+     * @param {string} [parent] - The parent of the block.
      */
-    constructor(title = "", description = "") {
-        super(title, description)
+    constructor(parent) {
+        super(parent)
     }
 }
 
@@ -153,11 +172,13 @@ class HomeGroup extends Group {
  */
 class Goal extends Block {
     /**
-     * @param {string} [title=""] - The title of the block.
-     * @param {string} [description=""] - The description of the block.
+     * @param {string} [parent] - The parent of the block.
      */
-    constructor(title = "", description = "") {
-        super(title, description)
+    constructor(parent) {
+        super(parent)
+        const idNum = blocks.get('goal-count')
+        this.id = 'goal-' + idNum
+        blocks.set('goal-count', idNum + 1)
         this.completed = false
     }
 
@@ -167,7 +188,7 @@ class Goal extends Block {
         }
         let n = 0
         for (let i = 0; i < this.children.length; i++) {
-            n += this.children[i].completion
+            n += blocks.get(this.children[i]).completion
         }
         return (n / this.children.length)
     }
@@ -176,6 +197,25 @@ class Goal extends Block {
      * If Block has less than 2 children, create two children.
      */
     split() {
-        if (this.children.length < 2) this.children = [new Goal(), new Goal()]
+        if (this.children.length < 2) {
+            this.addGoal()
+            this.addGoal()
+        }
+    }
+
+    /**
+     * Creates a tree of a certain depth under this block.
+     * @param {number} depth The depth of children under this block.
+     * @param {number|undefined} width The count of children each goal will have. Default is 2, must be above 0.
+     */
+    goalTree(depth, width = 2) {
+        if (width > 0 && depth > 0) {
+            this.removeChildren()
+            for (let i = 0; i < width; i++) {
+                this.addGoal()
+            }
+        }
     }
 }
+
+blocks.set('block-home', new HomeGroup())
