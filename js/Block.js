f94c87417d5b7cf98555d137546d852f7973e18d
Pootez <sondrenoe@gmail.com>
2023-07-02 20:43:15 +0200

    Remove comments

--- js/Block.js
+++ js/Block.js
@@ -39,45 +39,6 @@ class Block {
         return this.children.map(id => { blocks.get(id) })
     }
 
-    // /**
-    //  * Returns the entire family tree of the Block to a depth.
-    //  * @param {number|undefined} num The depth of the family tree, 0 returning just this block, negative numbers/undefined returning the entire tree.
-    //  * @returns {Array} The array of the tree to the leaf node, or given depth. [block, [block, leafBlock], leafBlock]
-    //  */
-    // getTree(num = -1) {
-    //     if (this.children.length == 0 || num == 0) {
-    //         return [this]
-    //     }
-    //     let arr = [this]
-    //     for (let i = 0; i < this.children.length; i++) {
-    //         arr.push(this.children[i].getTree(num - 1))
-    //     }
-    //     return arr
-    // }
-
-    // /**
-    // * Returns a branch of a family tree, given a list of child indexes.
-    // * @param {Array} arr The array of child indexes defining the family branch.
-    // * @param {number} depth The the current index in the depth search, starts at 0.
-    // * @returns {Array|undefined} The array of children corresponding to the child indexes (starting at this block), or undefined if one or more of the indexes are invalid
-    // */
-    // getBranch(arr, depth) {
-    //     if (arr.length <= 0 || depth >= arr.length) {
-    //         return [this]
-    //     }
-    //     const index = arr[depth]
-    //     if (index >= this.children.length || index < 0) {
-    //         return undefined
-    //     }
-    //     if (arr.length == 0) {
-    //         return [this.children[index]]
-    //     }
-    //     let branch = this.children[index].getBranch(arr, depth + 1)
-    //     if (!branch) return undefined
-    //     branch.unshift(this.children[index])
-    //     return branch
-    // }
-
     /**
      * @param {number} index The index of where the child is to be added. If undefined, it get's added at the end.
      */
