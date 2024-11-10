c051b3ce8b037f165bdaabb79bdb755fda4b20e0
Sondre Espeland <sondrenoe@gmail.com>
2024-11-10 12:31:12 +0100

    Reinstall using Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,8 @@
-import { Box, Heading, StackDivider } from '@chakra-ui/react'
-import { createContext, useState } from 'react'
-import TaskStack from './components/TaskStack'
 
-export interface Task {
-  id: number
-  title: string
-  children: (number|number[])[]
-  completed?: boolean
-  parallel?: boolean
-}
-
-const initialTasks = [
-  { id: 0, title: 'Task 1', children: [2, 3], },
-  { id: 1, title: 'Task 2', children: [3] },
-  { id: 2, title: 'Task 3', children: [] },
-  { id: 3, title: 'Task 4', children: [] },
-]
-
-export const TaskContext = createContext<{ tasks: Task[]; setTasks: any }>({
-  tasks: [],
-  setTasks: () => {},
-})
-
-const App = () => {
-  const [tasks, setTasks] = useState(initialTasks)
-  const childrenTasks = tasks
-    .map((task) => task.children)
-    .reduce((acc, children) => {
-      return [...acc, ...children]
-    })
-    .filter((id, index, self) => self.indexOf(id) === index)
+function App() {
 
   return (
-    <TaskContext.Provider value={{ tasks: tasks, setTasks: setTasks }}>
-      <Box p={5}>
-        <Heading
-        onClick={() => console.log('tasks', tasks)}
-        >1step2</Heading>
-        <br />
-        <TaskStack active parallel direction="row" divider={<StackDivider />} spacing={5}>
-          {tasks
-            .filter((task) => {
-              return childrenTasks.indexOf(task.id) === -1
-            })
-            .map((task) => task.id)}
-        </TaskStack>
-      </Box>
-    </TaskContext.Provider>
+    <h1>1step2</h1>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import theme from './theme.ts'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <App />
-    </ChakraProvider>
-  </React.StrictMode>
+    </ThemeProvider>
+  </StrictMode>,
 )
